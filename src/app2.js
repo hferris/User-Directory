2c6a41ec845c2f5f994ebd512ee477e52fe5919b
Hanna Ferris <hferris@dbq.edu>
2021-04-21 17:59:23 -0500

    setting up import

---
+++ src/app2.js
@@ -0,0 +1,24 @@
+import React from "react";
+import API from "./utils/API";
+​
+class App extends React.Component {
+  state = {
+    employees: [],
+  };
+​
+  componentDidMount() {
+    this.getEmployees();
+  }
+​
+  getEmployees = async () => {
+    const { data } = await API.getUsers();
+    this.setState({ employees: data.results });
+  };
+​
+  render() {
+    console.log(this.state);
+    return <h1>Employees go here</h1>;
+  }
+}
+​
+export default App;
