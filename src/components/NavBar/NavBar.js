de76e85c335894289ee77466eb720b87af618950
Hanna Ferris <hferris@dbq.edu>
2021-04-28 17:18:40 -0500

    working on NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,24 @@
 import React from "react";
+import context from "react-bootstrap/esm/AccordionContext";
+import "../css/NavBar.css";
 
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <form className="form-inline">
+        <input
+          className="form-control mr-sm-2"
+          aria-label="Search"
+          type="search"
+          placeholder="Search"
+          onChange={(event) => context.handleSearchChange(event)}
+        />
+        <button className="btn my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
 
-
-
-
-
-
-export default Navbar;
+export default NavBar;
