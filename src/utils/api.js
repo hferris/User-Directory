3ac8d482d930cbe7e5081ec4faa319c85d46e9da
Hanna Ferris <hferris@dbq.edu>
2021-04-28 17:45:26 -0500

    Making sure api matches all files

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const API = {
+const api = {
   // Gets all users
   getUsers: function() {
     return axios.get("https://randomuser.me/api/?results=200&nat=us");
   }
 };
-export default API;
+export default api;
